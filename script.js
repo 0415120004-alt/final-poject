f26d3557e18d8144b47a27c43f7b7eaae1a01877
0415120004-alt <0415120004@s.tusd1.org>
2026-04-29 18:50:00 +0000

    working on

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let petData = {
-    name: "Buddy",
-    hunger: 50,
-    happiness: 50,
-    energy: 50,
-    mood: "happy",
-    message: "Your pet is ready to play!"
-};
+function start(){
+    let secretNumber = Math.floor(Math.random() * 100) + 1;
+let numberOfGuesses = 0;
+
+function checkGuess() {
+    let playerGuess = document.getElementById("guessInput").value;
+    numberOfGuesses = numberOfGuesses + 1;
+    
+    if (playerGuess == secretNumber) {
+        // Show "You won!" message
+    } else if (playerGuess > secretNumber) {
+        // Show "Too high!" message
+    } else {
+        // Show "Too low!" message
+    }
+}
+
+}
+document.getElementById("guessInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        checkGuess();
+
+// Your variables go here
+let secretNumber = // use Math.random() to pick 1-100
+let numberOfGuesses = // start at 0
+
+// Function to check the guess
+function checkGuess() {
+    // Your code will go here
+}
+
+// Connect the button to the function
+// Use addEventListener here
+    }
+});
+
