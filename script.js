cc27ff262206aa07bee00c1411284752a738cad2
0415120004-alt <0415120004@s.tusd1.org>
2026-05-08 18:28:40 +0000

    complete

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-function start(){
-    let secretNumber = Math.floor(Math.random() * 100) + 1;
-let numberOfGuesses = 0;
+let secretNumber;
+let numberOfGuesses;
+
+function start() {
+    secretNumber = Math.floor(Math.random() * 100) + 1;
+    numberOfGuesses = 0;
+    document.getElementById("guessCount").innerText = "Guesses: 0";
+    document.getElementById("feedback").innerText = "";
+    document.getElementById("guessInput").value = "";
+}
 
 function checkGuess() {
     let playerGuess = document.getElementById("guessInput").value;
     numberOfGuesses = numberOfGuesses + 1;
     
+    let feedbackElement = document.getElementById("feedback");
+    let guessCountElement = document.getElementById("guessCount");
+    
+    guessCountElement.innerText = "Guesses: " + numberOfGuesses;
+
     if (playerGuess == secretNumber) {
-        // Show "You won!" message
-    } 
-    
+        feedbackElement.innerText = "You won! It took you " + numberOfGuesses + " guesses!";
+    }
     else if (playerGuess > secretNumber) {
-        // Show "Too high!" message
-    } 
-    
+        feedbackElement.innerText = "Too high! Try again.";
+    }
     else {
-        // Show "Too low!" message
+        feedbackElement.innerText = "Too low! Try again.";
     }
 }
 
-}
-document.getElementById("guessInput").addEventListener("keypress"), function(event) {
+start();
 
-let secretNumber = // use Math.random() to pick 1-100;
-let numberOfGuesses = // start at 0;
+let submitButton = document.getElementById("submitGuess");
+submitButton.addEventListener("click", checkGuess);
 
-function checkGuess() {
-    
-}
-    if (event.key === "Enter") {
-        checkGuess();
-    }
-}
-
+let playAgainButton = document.getElementById("playAgain");
+playAgainButton.addEventListener("click", start);
